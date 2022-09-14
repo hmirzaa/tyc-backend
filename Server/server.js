24e7e711292a6d1fdeffc45a4f113efaa50f96a5
Hamza Mughal <49321268+hmirzaa@users.noreply.github.com>
2022-09-14 23:40:23 +0500

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5500;
 require("dotenv").config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
